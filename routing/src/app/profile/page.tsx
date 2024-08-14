f9a309a59614f816ef05ba129f73b7df705628ff
Sachin K.C. <sachinxpml6@gmail.com>
2024-08-13 17:48:31 -0700

    routing in Nextjs completed

--- routing/src/app/profile/page.tsx
+++ routing/src/app/profile/page.tsx
@@ -5,16 +5,9 @@ import Image from "next/image";
 function page() {
   return (
     <div>
-      <Image
-        height={100}
-        width={100}
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvUpcpr2Yep3od01GQ-yHuGI-TkyKhoYuR3w&s"
-        alt="image"
-      />
       {<Script>{`alert("welcome to profile page ")`}</Script>}
       page
     </div>
   );
 }
-
 export default page;
